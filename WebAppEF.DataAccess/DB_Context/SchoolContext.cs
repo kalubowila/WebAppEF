e03fae84ca77c782372a74709f6d40a319aedf44
DESKTOP-2E8CL0V\T Kalubowila <kalubowila99@gmail.com>
2019-04-25 15:00:57 +0530

    Add project files.

---
+++ WebAppEF.DataAccess/DB_Context/SchoolContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using WebAppEF.Model.Entity;
+
+namespace WebAppEF.DataAccess.DB_Context
+{
+    public class SchoolContext : DbContext
+    {
+
+        public SchoolContext() : base("SchoolContext")  //connection string name
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
